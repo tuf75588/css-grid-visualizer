3c4b4bdc4f2f6d0b75b91f93ef6e83acc8903f13
Andrew Davis <atd285@gmail.com>
2021-07-12 14:03:21 -0400

    change image sizing based on grid toggle

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const input = document.querySelector('input[type="checkbox"]');
 const gridContainer = document.querySelector('.container');
+const img = document.querySelector('img');
 console.log(input);
 
 input.addEventListener('click', (e) => {
+  img.classList.toggle('img');
   const isChecked = e.target.checked;
   if (!isChecked) {
     gridContainer.style.display = 'block';
